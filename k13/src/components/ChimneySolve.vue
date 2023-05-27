6ed67b31585474868f0a92cfff145d686986cb90
kiralydaniel <kiralydani93@gmail.com>
2023-05-27 21:27:33 +0200

    feat: new ChimneySolve paragraphs

--- k13/src/components/ChimneySolve.vue
+++ k13/src/components/ChimneySolve.vue
@@ -13,14 +13,29 @@
           <p>2018 őszétől - jogszabályváltozás miatt - már csak kondenzációs kazánok szerelhetők fel az elromlott turbós
             (nem kondenzációs) kazánok helyett.</p>
         </div>
+        <div class="text-box-right">
+            <p>A kondenzációs kazánok füstgáza hidegebb a turbós kazánokénál, ezért az csak túlnyomással távozik maradéktalanul a kéményből.</p>
+
+            <p>Ha a meglévő kéményre kondenzációs és turbós kazán is bekötésre kerülne, a túlnyomás és a víz bejutna az éppen nem működő turbós kazánokba és tönkretenné azokat.</p>
+        </div>
       </div>
       <h2>Miért okoz ez problémát az érintett társasházaknak?</h2>
       <div class="text-box-row">
         <div class="text-box-left">
-          <p>A kazáncsatlakoztatásoknál beépítjük a speciális fütházcsappantyúkat, valamint kialakítjuk az alsó kondenzátumelvezetési pontot is.</p>
+          <p>Míg a turbós kazánok gyűjtőkéménye huzat elven működik, a <strong>kondenzációs kazánoknak túlnyomásos kéményre van szüksége.</strong> 
+            A meglévő turbós kémények ezt a két különböző igényt nem tudják egyszerre kiszolgálni.</p>
+
+            <p>A gyűjtőkéményeken keresztül a rá csatlakozó lakások összeköttetésben vannak egymással, ezért ha egy lakásban kazáncserére van szükség, az kötelezettséget ró a többi lakásra is.</p>
         </div>
-        <div class="text-box-right">
-          <p>Miután mérőműszerrel ellenőrizzük a megfelelő nyomásállóságot, felhelyezzük a membrános takarólemezeket, végül visszacsatlakoztatjuk a kazánokat.</p>
+      </div>
+      <h2>Mit lehet tenni ilyenkor?</h2>
+      <div class="text-box-row">
+        <div class="text-box-left">
+          <p>Kéményfelújítással meg kell oldani, hogy az új kondenzációs kazán együtt tudjon működni a régi turbós kazánokkal. A meglévő kéményt át kell alakítani olyan túlnyomásos, kondenzációs
+            gyűjtőkéménnyém, ami mind a magas hőmérsékletű (turbós), mind az alacsony hőmérsékletű és sok nedvességet tartalmazó (kondenzációs) égésterméknek ellenáll.
+            Így alkalmas lesz az újonnan felszerelt kondenzációs kazánok füstgáz elvezetésére is. Ezen felül speciális csappantyúk beépítésével meg kell akadályozni azt is, hogy a kondenzációs kazán
+            túlnyomása kárt tegyen a turbós készülékben.
+          </p>
         </div>
       </div>
     </div>
