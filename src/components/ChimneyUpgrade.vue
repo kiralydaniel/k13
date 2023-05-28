b2f27eebb6cd49b3a2cd4c5fd441707cad9548b7
kiralydaniel <kiralydani93@gmail.com>
2023-05-28 14:05:22 +0200

    refactor: mobile device text-box view

--- src/components/ChimneyUpgrade.vue
+++ src/components/ChimneyUpgrade.vue
@@ -7,17 +7,21 @@
     </h1>
     <div class="text-box-row">
       <div class="text-box-left">
+        <div class="circle">1</div>
         <p>A meglévő készülékek ideiglenes lecsatlakoztatásával lehetőve tesszük a kémény pontos felmérését és előkészítjük a bekötő nyílásokat az acél béléscső fogadására.</p>
       </div>
       <div class="text-box-right">
+        <div class="circle">2</div>
         <p>A felmért adatok alapján összeállítjuk teljes hosszában a bélésszerkezetet, melyet a kémény felső kitorkollásánál bevezetve nyerjük a bélelt kürőt.</p>
       </div>
     </div>
     <div class="text-box-row" ref="targetElement">
       <div class="text-box-left">
+        <div class="circle">3</div>
         <p>A kazáncsatlakoztatásoknál beépítjük a speciális füstgázcsappantyúkat, valamint kialakítjuk az alsó kondenzátumelvezetési pontot is.</p>
       </div>
       <div class="text-box-right">
+        <div class="circle">4</div>
         <p>Miután mérőműszerrel ellenőrizzük a megfelelő nyomásállóságot, felhelyezzük a membrános takarólemezeket, végül visszacsatlakoztatjuk a kazánokat.</p>
       </div>
     </div>
